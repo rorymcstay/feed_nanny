57e22422e7d1fbb6bfded91b27016fb2e1d425bf
Rory McStay <rory@rorymcstay.com>
2020-05-19 22:05:34 +0100

    start up logging improvements

--- nanny.py
+++ nanny.py
@@ -1,7 +1,6 @@
 import json
 import logging
-from logging import logging
-from logging import dictConfig
+from logging.config import dictConfig
 import os
 from queue import Queue
 from feed.settings import database_parameters, nanny_params, mongo_params
@@ -18,25 +17,7 @@ from src.main.domain import Feed
 from pymongo import MongoClient
 from datetime import timedelta
 
-logging.basicConfig(level=os.environ.get("LOGLEVEL", "DEBUG"))
-logging.getLogger("urllib3").setLevel("INFO")
 
-app = init_app(Feed)
-
-logging.info("####### Environment #######")
-
-
-logging.info("nanny: {}".format(json.dumps(nanny_params, indent=4, sort_keys=True)))
-
-
-
-ParameterController.register(app)
-MappingManager.register(app)
-Service.register(app)
-ActionsManager.register(app)
-SamplePages.register(app)
-RunningManager.register(app)
-CORS(app)
 
 if __name__ == '__main__':
     dictConfig({
@@ -45,8 +26,8 @@ if __name__ == '__main__':
             'format': '[%(asctime)s]%(thread)d: %(module)s - %(levelname)s - %(message)s',
         }},
         'handlers': {'wsgi': {
-            'class': 'logging.FileHandler',
-            'filename': f'{os.getenv("DEPLOYMENT_ROOT")}/tmp/ci.log',
+            'class': 'logging.StreamHandler',
+            'stream': 'ext://flask.logging.wsgi_errors_stream',
             'formatter': 'default'
         }},
         'root': {
@@ -55,6 +36,22 @@ if __name__ == '__main__':
         }
     })
 
+    logging.info("####### Environment #######")
+
+
     logging.info("\n".join([f'{key}={os.environ[key]}' for key in os.environ]))
-    print(app.url_map)
+    logging.info("nanny: {}".format(json.dumps(nanny_params, indent=4, sort_keys=True)))
+    logging.basicConfig(level=os.environ.get("LOGLEVEL", "DEBUG"))
+    logging.getLogger("urllib3").setLevel("INFO")
+
+    app = init_app(Feed)
+    ParameterController.register(app)
+    MappingManager.register(app)
+    Service.register(app)
+    ActionsManager.register(app)
+    SamplePages.register(app)
+    RunningManager.register(app)
+    CORS(app)
+
+    logging.info(app.url_map)
     app.run(port=os.getenv("FLASK_PORT", os.getenv("NANNY_PORT", 5003)), host=os.getenv('NANNY_HOST'))
