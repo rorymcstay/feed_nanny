7f7043fecb4475fb826850f526817da3c1b60b58
Rory McStay <rory@rorymcstay.com>
2020-05-19 21:46:29 +0100

    logging improvements

--- nanny.py
+++ nanny.py
@@ -1,5 +1,7 @@
 import json
 import logging
+from logging import logging
+from logging import dictConfig
 import os
 from queue import Queue
 from feed.settings import database_parameters, nanny_params, mongo_params
@@ -37,6 +39,22 @@ RunningManager.register(app)
 CORS(app)
 
 if __name__ == '__main__':
+    dictConfig({
+        'version': 1,
+        'formatters': {'default': {
+            'format': '[%(asctime)s]%(thread)d: %(module)s - %(levelname)s - %(message)s',
+        }},
+        'handlers': {'wsgi': {
+            'class': 'logging.FileHandler',
+            'filename': f'{os.getenv("DEPLOYMENT_ROOT")}/tmp/ci.log',
+            'formatter': 'default'
+        }},
+        'root': {
+            'level': 'INFO',
+            'handlers': ['wsgi']
+        }
+    })
+
     logging.info("\n".join([f'{key}={os.environ[key]}' for key in os.environ]))
     print(app.url_map)
     app.run(port=os.getenv("FLASK_PORT", os.getenv("NANNY_PORT", 5003)), host=os.getenv('NANNY_HOST'))
