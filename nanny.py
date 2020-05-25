328d8c102b3f8d50d3d88ea874602235291bfeeb
Rory McStay <rory@rorymcstay.com>
2020-05-25 01:15:40 +0100

    bumping requirements adn improve logging

--- nanny.py
+++ nanny.py
@@ -3,7 +3,7 @@ from logging.config import dictConfig
 import os
 from feed.settings import nanny_params, mongo_params
 
-from src.main.app import app
+
 
 
 if __name__ == '__main__':
@@ -28,6 +28,8 @@ if __name__ == '__main__':
     logging.info("####### Environment #######")
     logging.info(f'nanny: {nanny_params}, mongo: {mongo_params}')
     logging.info("\n".join([f'{key}={os.environ[key]}' for key in os.environ]))
+
+    from src.main.app import app
     logging.info(app.url_map)
 
     app.run(port=os.getenv("FLASK_PORT", os.getenv("NANNY_PORT", 5003)), host=os.getenv('NANNY_HOST'))
